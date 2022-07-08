66fdae18e54bcc2af129875df50c46efc4f84adb
dhythm <yuta.okada.20@gmail.com>
2022-07-09 01:51:13 +0900

    update index.tsx

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,22 +1,78 @@
 /** @jsx h */
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Head } from "$fresh/src/runtime/head.ts";
 import { tw } from "@twind";
+import "dayjs/locale/ja";
+import relativeTime from "dayjs/plugin/relativeTime";
+import dayjs from "https://esm.sh/dayjs@1.11.3";
 import { h } from "preact";
-import Counter from "../islands/Counter.tsx";
+dayjs.extend(relativeTime);
+dayjs.locale("ja");
 
-export default function Home() {
+interface Article {
+  id: string;
+  title: string;
+  created_at: string;
+}
+
+export const handler: Handlers<Article[]> = {
+  async GET(_, ctx) {
+    const articles: Article[] = [
+      {
+        id: "1",
+        title: "Article 1",
+        created_at: "2022-06-17T00:00:00.000Z",
+      },
+      {
+        id: "2",
+        title: "Article 2",
+        created_at: "2022-06-10T00:00:00.000Z",
+      },
+    ];
+    return ctx.render(articles);
+  },
+};
+
+export default function Home({ data }: PageProps<Article[]>) {
   return (
-    <div>
-      <h1 class={tw("text-red-500")}>Header</h1>
-      <img
-        src="/logo.svg"
-        height="100px"
-        alt="the fresh logo: a sliced lemon dripping with juice"
-      />
-      <p>
-        Welcome to `fresh`. Try update this message in the ./routes/index.tsx
-        file, and refresh.
-      </p>
-      <Counter start={3} />
+    <div class={tw("h-screen bg-gray-200")}>
+      <Head>
+        <title>Fresh Blog</title>
+      </Head>
+      <div
+        class={tw(
+          "max-w-screen-sm mx-auto px-4 sm:px-6 md:px-8 pt-12 pb-20 flex flex-col"
+        )}
+      >
+        <h1 class={tw("font-extrabold text-5xl text-gray-800")}>Fresh Blog</h1>
+        <section class={tw("mt-8")}>
+          <h2 class={tw("text-4xl font-bold text-gray-800 py-4")}>Posts</h2>
+          <ul>
+            {data.map((article) => (
+              <li
+                class={tw("bg-white p-6 rounded-lg shadow-lg mb-4")}
+                key={article.id}
+              >
+                <a href={`articles/${article.id}`}>
+                  <h3
+                    class={tw(
+                      "text-2xl font-bold mb-2 text-gray-800 hover:text-gray-600 hover:text-underline"
+                    )}
+                  >
+                    {article.title}
+                  </h3>
+                </a>
+                <time
+                  class={tw("text-gray-500 text-sm")}
+                  dateTime={article.created_at}
+                >
+                  {dayjs(article.created_at).fromNow()}
+                </time>
+              </li>
+            ))}
+          </ul>
+        </section>
+      </div>
     </div>
   );
 }
